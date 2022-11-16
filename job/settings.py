20f665e8f88e7a735dc17db575f03a25e12666c1
Nrinconv <nrinconv@unal.edu.co>
2022-11-16 09:53:29 -0500

    Probando heroku modificando django heroku

--- job/settings.py
+++ job/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +22,7 @@ SECRET_KEY = 'etdq)uvq=t0rc&ams5_ovn6w8bcwknjj0u97*(#n^(76x*+dr1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', '127.0.0.1', 'rss-feed-project.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1:8000, rss-feed-project.herokuapp.com']
 
 
 # Application definition
@@ -170,6 +171,7 @@ MESSAGE_TAGS = {
 
 
 # Activate Django-Heroku.
+django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
