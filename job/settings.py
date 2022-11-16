ee36f023fb7516ae1cccda80c148bf78c0c94210
Nrinconv <nrinconv@unal.edu.co>
2022-11-15 22:07:49 -0500

    Database checkeada

--- job/settings.py
+++ job/settings.py
@@ -81,21 +81,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'job.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'jobportal',
-#         'USER': 'postgres',
-#         'PASSWORD': '1234',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
